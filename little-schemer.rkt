91f9fb540bd27cac6d7da3ea469db172e89ce17f
guoj <dev.guoj@gmail.com>
2017-03-06 15:04:03 -0700

    some new code

--- little-schemer.rkt
+++ little-schemer.rkt
@@ -105,12 +105,75 @@
     (cond
       ((zero? m) n)
       (else (o+ (sub1 m) (add1 n))))))
-    ; (else (add1 (o+ n (sub1 m)))))))
+; (else (add1 (o+ n (sub1 m)))))))
 
 (define o-
   (lambda (m n)
     (cond
       ((zero? n) m)
       (else (o- (sub1 m) (sub1 n))))))
-    ; (else (sub1 (o- n (sub1 m)))))))
+; (else (sub1 (o- n (sub1 m)))))))
+
+(define addtup
+  (lambda (tup)
+    (cond
+      ((null? tup) 0)
+      (else (o+ (car tup) (addtup (cdr tup)))))))
+
+(define o*
+  (lambda (m n)
+    (cond
+      ((zero? m) 0)
+      (else (o+ n (o* (sub1 m) n))))))
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond
+      ((null? tup1) tup2)
+      ((null? tup2) tup1)
+      (else (cons (o+ (car tup1) (car tup2))
+                  (tup+ (cdr tup1) (cdr tup2)))))))
+
+(define o>
+  (lambda (m n)
+    (cond
+      ((zero? m) #f)
+      ((zero? n) #t)
+      (else (o> (sub1 m) (sub1 n))))))
+
+(define o<
+  (lambda (m n)
+    (cond
+      ((zero? n) #f)
+      ((zero? m) #t)
+      (else (o< (sub1 m) (sub1 n))))))
+
+(define o=
+  (lambda (m n)
+    (and (not (o> m n))
+         (not (o< m n)))))
+
+(define power
+  (lambda (m n)
+    (cond
+      ((zero? n) 1)
+      (else (o* m (power m (sub1 n)))))))
+
+(define o/
+  (lambda (m n)
+    (cond
+      ((o< m n) 0)
+      (else (add1 (o/ (- m n) n))))))
+
+(define len
+  (lambda (lat)
+    (cond
+      ((null? lat) 0)
+      (else (add1 (len (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((o=)))))
+
 
